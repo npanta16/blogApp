eb9d89e92b286a8e1d84916ba211175ab29a93c9
npanta16 <npanta16@gmail.com>
2023-01-08 21:52:39 +1100

    creating login page for blog app

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/home/Home'
 import Setting from './pages/setting/Setting'
 import Single from './pages/single/Single'
 import Write from './pages/write/Write'
+import Login from './pages/login/Login'
 
 function App() {
   return (
@@ -12,7 +13,9 @@ function App() {
     {/*<Home/> */} 
     {/*<Single/> */} 
     {/* <Write/> */} 
-    <Setting/>
+     {/* <Setting/> */} 
+     <Login/>
+    
  
     
     </div>
