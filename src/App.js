e177142ec8b6a0bcbe470820fc98305e69fe2b7a
npanta16 <npanta16@gmail.com>
2023-01-08 18:49:49 +1100

    creating writing page for writing blog

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react'
 import TopBar from './components/topbar/TopBar'
 import Home from './pages/home/Home'
 import Single from './pages/single/Single'
+import Write from './pages/write/Write'
 
 function App() {
   return (
     <div>
      <TopBar/>
     {/*<Home/> */} 
-    <Single/>
+    {/*<Single/> */} 
+    <Write/>
+    
     </div>
   )
 }
